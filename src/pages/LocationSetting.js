adc25d50c69aac379bbf5f3b0b285fb7e38e3e4f
jeje70 <chipupupu7@gmail.com>
2021-01-17 19:57:36 +0900

    [add] footer name, location with css

--- src/pages/LocationSetting.js
+++ src/pages/LocationSetting.js
@@ -1,3 +1,4 @@
+import '../style/LocationSetting.css';
 import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form';
 //import Button from 'react-bootstrap/Button';
@@ -11,7 +12,8 @@ export default class LocationSetting extends Component {
                 <Form>
                     <Form.Group controlId="location-state">
                         <Form.Label>도</Form.Label>
-                        <Form.Control as="select" custom>
+                        <br/>
+                        <Form.Control style={{width: '300px'}} as="select" custom>
                             <option>서울</option>
                             <option>부산</option>
                             <option>진주</option>
@@ -22,7 +24,8 @@ export default class LocationSetting extends Component {
                 <Form>
                     <Form.Group controlId="location-city">
                         <Form.Label>시</Form.Label>
-                        <Form.Control as="select" custom>
+                        <br/>
+                        <Form.Control style={{width: '300px'}} as="select" custom>
                             <option>서울</option>
                             <option>부산</option>
                             <option>진주</option>
