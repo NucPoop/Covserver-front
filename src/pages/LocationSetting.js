79803b5138585b07f50a4844dbe9967e5445c492
jeje70 <chipupupu7@gmail.com>
2021-03-05 20:59:57 +0900

    [trivial] try to fetch

--- src/pages/LocationSetting.js
+++ src/pages/LocationSetting.js
@@ -2,9 +2,31 @@ import '../style/LocationSetting.css';
 import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+//import axios from 'axios';
 //import Button from 'react-bootstrap/Button';
 
 export default class LocationSetting extends Component {
+    // constructor() {
+    //     super();
+
+    //     const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
+    //     fetch('https://www.juso.go.kr/addrlink/addrLinkApi.do', {
+    //         method: "get",
+    //         params: {
+    //             confmKey: "devU01TX0FVVEgyMDIxMDMwNDExNDg0MDExMDg3OTc=",
+    //             keyword: "siNm",
+    //             resultType: "json"
+    //         },
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //             'Accept': 'applictation/json'
+    //         }
+    //     })
+    //         .then(response => response.json())
+    //         .then(data => console.log(data));
+    // }
+
     render() {
         return (
             <div className="location-setting">
@@ -13,7 +35,7 @@ export default class LocationSetting extends Component {
                 <Form>
                     <Form.Group controlId="location-state">
                         <Form.Label>도</Form.Label>
-                        <br/>
+                        <br />
                         <Form.Control className="province" as="select" custom>
                             <option>서울</option>
                             <option>부산</option>
@@ -25,7 +47,7 @@ export default class LocationSetting extends Component {
                 <Form>
                     <Form.Group className="city" controlId="location-city">
                         <Form.Label>시</Form.Label>
-                        <br/>
+                        <br />
                         <Form.Control as="select" custom>
                             <option>서울</option>
                             <option>부산</option>
@@ -38,12 +60,12 @@ export default class LocationSetting extends Component {
 
                 <div className="alarm-setting">
                     <h3>알림 설정</h3>
-                    
+
                     <Form>
                         <Form.Group className="alarm" controlId="set-alarm">
                             <Form.Label>이메일</Form.Label>
-                            <br/>
-                            <Form.Control type="email"/>
+                            <br />
+                            <Form.Control type="email" />
                         </Form.Group>
                     </Form>
 
