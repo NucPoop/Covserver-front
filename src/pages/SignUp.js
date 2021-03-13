413853688b39821ea5a126a5e00ceb9756e30e6f
Nucpoop <nucpoop@Jam-Mac-mini.local>
2021-03-14 01:07:06 +0900

    [add] check email signup

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,11 @@
 import '../style/SignUp.css';
 import React, { Component } from "react";
 import Button from 'react-bootstrap/Button';
-import { signup } from './components/UserAPIs';
+import { signup, checkEmailAvailability } from './components/UserAPIs';
 
 export default class SignUp extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.SignUp = this.SignUp.bind(this);
         this.checkMsg = React.createRef();
@@ -24,8 +24,8 @@ export default class SignUp extends Component {
 
     handleChangeEmail = input => {
         this.setState({
-            email : {
-                value:input.target.value
+            email: {
+                value: input.target.value
             }
         });
     };
@@ -33,7 +33,7 @@ export default class SignUp extends Component {
     handleChangePassword = input => {
         this.setState({
             password: {
-                value:input.target.value
+                value: input.target.value
             }
         });
 
@@ -48,13 +48,20 @@ export default class SignUp extends Component {
             password: this.state.password.value
         };
 
-        signup(signupRequest)
-        .then(response => {
-            alert("회원가입 완료, 로그인 해주세요.");
-            this.props.history.push("/sign-in");
-        }).catch(error => {
-            alert("회원가입 실패");
-        });
+        checkEmailAvailability(this.state.email.value)
+            .then(response => {
+                if (response.canEmailSignUp) {
+                    signup(signupRequest)
+                        .then(response => {
+                            alert("회원가입 완료, 로그인 해주세요.");
+                            this.props.history.push("/sign-in");
+                        }).catch(error => {
+                            alert("회원가입 실패");
+                        });
+                } else {
+                    alert("중복된 이메일입니다.");
+                }
+            });
     }
 
     render() {
@@ -66,12 +73,12 @@ export default class SignUp extends Component {
 
                 <div className="form-group">
                     <label>이메일</label>
-                    <input type="email" className="form-control" placeholder="Enter email" required onChange={this.handleChangeEmail} value={email}/>
+                    <input type="email" className="form-control" placeholder="Enter email" required onChange={this.handleChangeEmail} value={email} />
                 </div>
 
                 <div className="form-group">
                     <label>비밀번호</label>
-                    <input type="password" className="form-control" placeholder="Enter password" required onChange={this.handleChangePassword} value={password}/>
+                    <input type="password" className="form-control" placeholder="Enter password" required onChange={this.handleChangePassword} value={password} />
                 </div>
 
                 <p ref={this.checkMsg} hidden={this.state.isValid} className="check-message"> 이미 사용 중인 이메일입니다. </p>
@@ -86,14 +93,14 @@ export default class SignUp extends Component {
     }
 
     validatePassword = (password) => {
-        if(password.length < 4) {
-            this.checkMsg.current.innerHTML="4자 이상 입력해주세요.";
-            this.checkMsg.current.hidden=false;
+        if (password.length < 4) {
+            this.checkMsg.current.innerHTML = "4자 이상 입력해주세요.";
+            this.checkMsg.current.hidden = false;
         } else if (password.length > 20) {
-            this.checkMsg.current.innerHTML="20자 이하 입력해주세요.";
-            this.checkMsg.current.hidden=false;
+            this.checkMsg.current.innerHTML = "20자 이하 입력해주세요.";
+            this.checkMsg.current.hidden = false;
         } else {
-            this.checkMsg.current.hidden=true;
+            this.checkMsg.current.hidden = true;
         }
     }
 }
