577ce66ebc8b747cb7c58068ab95d0c210ca1959
Nucpoop <kjm5546@gmail.com>
2021-06-18 00:32:29 +0900

    [fix] backend port

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,7 +1,7 @@
 export { default as Home } from './Home';
 export { default as Login } from './Login';
 
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://jamjam.hopto.org:8180/api';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://jamjam.hopto.org:5180/api';
 //export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8180/api';
 export const ACCESS_TOKEN = 'accessToken';
 
